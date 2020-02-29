f653acdbd2f1bddafcc4e2ad98fec118b91338f9
alexger95 <alexger95@gmail.com>
2020-02-29 16:49:26 +0300

    init: v1 solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) n = n * (-1);
+    n = n.toString();
+    n = n.split('').reverse().join('');
+
+    return +n;
 }
